933deb033b740cc5717b14a73bb339bc682fb96f
Yury Salagaev <y.salagaev@12storeez.com>
2021-02-04 12:47:25 +0700

    spring boot instance

---
+++ src/main/java/com/irc/JStrongBody/JStrongBodyApplication.java
@@ -0,0 +1,13 @@
+package com.irc.JStrongBody;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JStrongBodyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JStrongBodyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/irc/JStrongBody/JStrongBodyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.irc.JStrongBody;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JStrongBodyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
